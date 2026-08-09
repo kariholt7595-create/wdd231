fc8daa0e5aa6c5be89ee6c5c2937d919e2aac2ad
kariholt7595-create <kariholt7595@gmail.com>
2026-08-08 23:27:19 -0600

    second webpage for final complete

---
+++ final/scripts/activities.js
@@ -0,0 +1,81 @@
+const activityCards = document.querySelector("#activity-cards");
+const activityDialog = document.querySelector("#activity-dialog");
+const dialogContent = document.querySelector("#dialog-content");
+const closeDialog = document.querySelector("#close-dialog");
+const filterButtons = document.querySelectorAll(".activity-filters button")
+let allActivities = [];
+
+async function getActivities() {
+    try {
+        const response = await fetch("data/activities.json");
+        const activities = await response.json();
+        allActivities = activities;
+
+        console.log(activities);
+        displayActivities(activities);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getActivities();
+
+function displayActivities(activities) {
+    activityCards.innerHTML = "";
+
+    activities.forEach((activity) => {
+        const card = document.createElement("section");
+        card.classList.add("activity-card");
+        card.classList.add(activity.category);
+
+        const title = document.createElement("h2");
+        title.textContent = activity.title;
+
+        const category = document.createElement("p");
+        category.textContent = activity.category;
+
+        const categoryCircle = document.createElement("div");
+        categoryCircle.classList.add("category-circle");
+        categoryCircle.textContent = activity.category.charAt(0).toUpperCase();
+
+        const time = document.createElement("p");
+        time.textContent = activity.time;
+
+        const description = document.createElement("p");
+        description.textContent = activity.description;
+
+        const learnMore = document.createElement("button")
+        learnMore.textContent = "Learn More";
+
+        learnMore.addEventListener("click", () => {
+            dialogContent.textContent = activity.details;
+            activityDialog.showModal();
+        });
+
+        card.appendChild(categoryCircle);
+        card.appendChild(title);
+        card.appendChild(time);
+        card.appendChild(learnMore);
+
+        activityCards.appendChild(card);
+    });
+
+    closeDialog.addEventListener("click", () => {
+        activityDialog.close();
+    })
+
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const category = button.dataset.category;
+            if (category === "all") {
+                displayActivities(allActivities);
+            } else {
+                const filteredActivities = allActivities.filter((activity) =>
+                    activity.category === category
+                );
+
+                displayActivities(filteredActivities);
+            }
+        });
+    });
+}
